c325bcd70811249109fc6d88b16c46fbac291266
Anaïs Gilbert <a.gilbertt3@hotmail.com>
2022-05-21 00:05:51 +0100

    added animations

---
+++ app.js
@@ -0,0 +1,17 @@
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener('click', () => {
+    container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener('click', () => {
+    container.classList.remove("sign-up-mode");
+});
+
+
+
+
+// add a class to the container element when signup button on the panel is clicked
+// remove the class when sign in is clicked on the other panel
